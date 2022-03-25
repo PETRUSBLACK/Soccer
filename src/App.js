e4beccd476a99e1081e5863bf37ba60124a92647
PETRUSBLACK <45966588+PETRUSBLACK@users.noreply.github.com>
2022-03-25 18:05:27 +0100

    testing the heroku

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-// import Login from './Components/logIn/login'
-// import CreateAccount from './Components/create-account/createAcc'
+import Login from './Components/logIn/login'
+import CreateAccount from './Components/create-account/createAcc'
 import Otp from './Components/OTP/otp';
 
 function App() {
   return (
     <>
-      {/* <Login /> */}
-      {/* <CreateAccount /> */}
+      <Login />
+      <CreateAccount />
       <Otp />
     </>
   );
